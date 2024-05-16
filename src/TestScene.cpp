40f80190224db576a1592a531bb37d79441cbdac
Smiley98 <smileysofficialcinema@gmail.com>
2024-05-16 16:19:57 -0400

    Fixed fract to match documentation

--- src/TestScene.cpp
+++ src/TestScene.cpp
@@ -1,5 +1,25 @@
 #include "TestScene.h"
 #include "Shader.h"
+#include "App.h"
+
+float Fract(float f)
+{
+	return f - floorf(f);
+}
+
+Color3 Palette(float t) {
+	Color3 a { 0.5f, 0.5f, 0.5f };
+	Color3 b { 0.5f, 0.5f, 0.5f };
+	Color3 c { 1.0f, 1.0f, 1.0f };
+	Color3 d{ 0.263f, 0.416f, 0.557f };
+
+	Color3 cd = (c * t + d) * 6.28318f;
+	cd.r = cosf(cd.r);
+	cd.g = cosf(cd.g);
+	cd.b = cosf(cd.b);
+
+	return a + b * cd;
+}
 
 void TestScene::OnLoad()
 {
@@ -16,6 +36,10 @@ void TestScene::OnUnload()
 
 void TestScene::OnUpdate(float dt)
 {
+	float tt = TotalTime();
+	float ncos = cosf(tt) * 0.5f + 0.5f;
+	Color3 bg = Palette(ncos);
+
 	for (int i = 0; i < mImage.height; i++)
 	{
 		for (int j = 0; j < mImage.width; j++)
@@ -26,12 +50,13 @@ void TestScene::OnUpdate(float dt)
 			uv = uv * 2.0f - 1.0f;
 			uv.x *= SCREEN_ASPECT;
 
-			float distance = Length(uv);
-			Color color = distance > 1.0f ? WHITE : BLACK;
+			uv.x = Fract(uv.x * 2.0f);
+			uv.y = Fract(uv.y * 2.0f);
+
+			Color color = Length(uv) > 1.0 ? WHITE : BLACK;
 			mImage.pixels[i * mImage.width + j] = color;
 		}
 	}
-	Flip(mImage);
 	UpdateTexture(mTexture, mImage);
 }
 
