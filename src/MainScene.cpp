18dfca2963d6d7656d6213cb04b4308bd9e59736
Smiley98 <smileysofficialcinema@gmail.com>
2024-08-21 13:50:54 -0400

    Removed manual vertex copying

--- src/MainScene.cpp
+++ src/MainScene.cpp
@@ -8,19 +8,6 @@
 
 constexpr int IMAGE_SIZE = 512;
 
-Vector3* CopyPositions(Vector3* vertices, size_t vertexCount)
-{
-	Vector3* positions = new Vector3[vertexCount];
-	memcpy(positions, vertices, sizeof(Vector3) * vertexCount);
-	return positions;
-}
-
-void TransformPositions(Matrix matrix, Vector3* vertices, size_t vertexCount)
-{
-	for (size_t i = 0; i < vertexCount; i++)
-		vertices[i] = matrix * vertices[i];
-}
-
 void MainScene::OnLoad()
 {
 	LoadImage(&mImage, IMAGE_SIZE, IMAGE_SIZE);
