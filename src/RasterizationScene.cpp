a624f4ab5285d70143e650f7faf8e2d33e64a45b
Smiley98 <smileysofficialcinema@gmail.com>
2024-08-26 18:35:35 -0400

    Made a deranged render

--- src/RasterizationScene.cpp
+++ src/RasterizationScene.cpp
@@ -10,15 +10,18 @@ void RasterizationScene::OnDraw()
 
 	Matrix translation = MatrixIdentity();//Translate(cos(tt), 0.0f, 0.0f);
 	Matrix rotation = RotateY(tt * 100.0f * DEG2RAD);
-	Matrix scale = MatrixIdentity();//Scale(cos(tt) * 0.4f + 0.6f, sin(tt) * 0.4f + 0.6f, 1.0f);
+	Matrix scale = Scale(5.0f, 5.0f, 5.0f);//MatrixIdentity();//Scale(cos(tt) * 0.4f + 0.6f, sin(tt) * 0.4f + 0.6f, 1.0f);
 
 	Matrix world = scale * rotation * translation;
-	Matrix view = MatrixIdentity();//LookAt({ 0.0f, 0.0f, 5.0f }, V3_ZERO, V3_UP);
-	Matrix proj = MatrixIdentity();//Perspective(75.0f * DEG2RAD, 1.0f, 0.001f, 100.0f);
+	Matrix view = LookAt({ 0.0f, 0.0f, 5.0f }, V3_ZERO, V3_UP);
+	//Matrix proj = Perspective(75.0f * DEG2RAD, 1.0f, 0.001f, 100.0f);
+	Matrix proj = Ortho(-10.0f, 10.0f, -10.0f, 10.0f, -10.0f, 10.0f);
 	Matrix mvp = world * view * proj;
 
 	DrawMeshWireframes(gMeshTriangle, mvp, { 1.0f, 0.0f, 0.0f });
 	DrawMeshWireframes(gMeshCube, mvp, { 0.0f, 1.0f, 0.0f });
 	DrawMeshWireframes(gMeshSphere, mvp, { 0.0f, 0.0f, 1.0f });
-	//DrawMeshNormals(gMeshCube, mvp, world);
+	DrawMeshNormals(gMeshHead, mvp, world);
+
+	// TODO -- create sphere, capsule, and box rendering functions via par_shapes
 }
