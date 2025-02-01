32acf0b23c170c74dcbf281dbc47f28a856db1c5
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-31 22:33:55 -0500

    Minor changes to CPU phong scene

--- src/MainScene.cpp
+++ src/MainScene.cpp
@@ -4,12 +4,10 @@
 #include "Camera.h"
 #include "Time.h"
 
-static Mesh fMesh;
 static Image fDiffuseMap;
 
 void MainScene::OnLoad()
 {
-	fMesh = gMeshHead;
 	gCamera = FromView(LookAt({ 0.0f, 0.0f, 5.0f }, V3_ZERO, V3_UP));
 	CreateImageFromFile(&fDiffuseMap, "assets/textures/african_head_diffuse.png", true);
 }
@@ -29,13 +27,12 @@ void MainScene::OnUpdate(float dt)
 	float ncos = cosf(tt) * 0.5f + 0.5f;
 
 	Matrix translation = MatrixIdentity();//Translate(cos(tt), 0.0f, 0.0f);
-	Matrix rotation = RotateY(tt * 100.0f * DEG2RAD);
+	Matrix rotation = MatrixIdentity();//RotateY(tt * 100.0f * DEG2RAD);
 	Matrix scale = MatrixIdentity();//Scale(cos(tt) * 0.4f + 0.6f, sin(tt) * 0.4f + 0.6f, 1.0f);
 	UpdateFpsCameraDefault(gCamera, dt);
 
 	Matrix model = scale * rotation * translation;
 	Matrix view = ToView(gCamera);
-	//Matrix view = LookAt({ 0.0f, 0.0f, 5.0f }, V3_ZERO, V3_UP);
 	Matrix proj = Perspective(75.0f * DEG2RAD, 1.0f, 0.001f, 100.0f);
 	Matrix mvp = model * view * proj;
 
@@ -53,10 +50,10 @@ void MainScene::OnUpdate(float dt)
 	Vector3 c{ 1.0f, 1.0f, 1.0f };
 	Vector3 d{ 0.263f, 0.416f, 0.557f };
 
-	data.lightColor = V3_ONE;//Palette(a, b, c, d, cosf(tt) * 0.5f + 0.5f);
+	data.lightColor = Palette(a, b, c, d, cosf(tt) * 0.5f + 0.5f);
 	data.ambient = 0.25f;
 	data.diffuse = 0.75f;
 	data.specular = 32.0f;
 
-	DrawMesh(&gImageCPU, fMesh, data, &fDiffuseMap);
+	DrawMesh(&gImageCPU, gMeshHead, data, &fDiffuseMap);
 }
