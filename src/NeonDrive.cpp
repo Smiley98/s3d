92b54099edc46bf743a40f335407b9ebcb465795
Smiley98 <smileysofficialcinema@gmail.com>
2025-02-02 23:57:38 -0500

    Making progress with deferred lighting

--- src/NeonDrive.cpp
+++ src/NeonDrive.cpp
@@ -7,7 +7,6 @@
 
 static Texture2D fTextureWhite;
 static Texture2D fTextureAlbedo;
-static Texture2D fTextureGround;
 static Framebuffer fGeometryBuffer;
 
 static Vector3 fLightPosition = V3_ZERO;
@@ -15,7 +14,7 @@ static Vector3 fLightColor = V3_ONE;
 
 void NeonDriveScene::OnLoad()
 {
-	gCamera = FromView(LookAt({ 0.0f, 0.0f, 50.0f }, V3_ZERO, V3_UP));
+	gCamera = FromView(LookAt({ 0.0f, 0.0f, 100.0f }, V3_ZERO, V3_UP));
 
 	// World's most elaborate texture xD xD xD
 	{
@@ -31,14 +30,6 @@ void NeonDriveScene::OnLoad()
 		UnloadImage(pixels);
 	}
 
-	// Original format = RGB
-	//{
-	//	int w, h, c;
-	//	uint8_t* pixels = LoadImage2D("./assets/textures/ground.png", &w, &h, &c);
-	//	CreateTexture2D(&fTextureGround, w, h, GL_RGB8, GL_RGB, GL_UNSIGNED_BYTE, GL_LINEAR, pixels);
-	//	UnloadImage(pixels);
-	//}
-
 	// Positions, normals, and albedo are all 3-component. Can convert to RGBA for albedo-specular.
 	CreateFramebuffer(&fGeometryBuffer, SCREEN_WIDTH, SCREEN_HEIGHT);
 	AddColor(&fGeometryBuffer, GL_RGB16F, GL_RGB, GL_FLOAT, GL_NEAREST);		// Positions
@@ -51,7 +42,6 @@ void NeonDriveScene::OnLoad()
 void NeonDriveScene::OnUnload()
 {
 	DestroyFramebuffer(&fGeometryBuffer);
-	DestroyTexture2D(&fTextureGround);
 	DestroyTexture2D(&fTextureAlbedo);
 	DestroyTexture2D(&fTextureWhite);
 }
@@ -63,39 +53,42 @@ void NeonDriveScene::OnUpdate(float dt)
 	gProj = Perspective(PI * 0.5f, SCREEN_ASPECT, 0.1f, 1000.0f);
 
 	float tt = TotalTime();
-	fLightPosition = { cosf(tt) * 10.0f, 0.0f, 0.0f };
+	fLightPosition = { cosf(tt) * 10.0f, 0.0f, 25.0f };
 }
 
 void NeonDriveScene::OnDraw()
 {
-	SetPipelineState(gPipelineNoDepth);
-	DrawMeshTexture(gMeshPlane, Scale(100.0f * SCREEN_ASPECT, 100.0f, 1.0f), GetHexagonGrid(), 0);
-	SetPipelineState(gPipelineDefault);
-	return;
-
-	// TODO -- Add buildings!
-	Matrix world = MatrixIdentity();//Scale(V3_ONE * 10.0f);
-	Matrix mvp = world * gView * gProj;
+	Matrix world = MatrixIdentity();
+	Matrix mvp = MatrixIdentity();
 
 	BindFramebuffer(fGeometryBuffer);
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// TODO -- Save hex grid to textore on-start, then sample from it (since g-buffer must remain bound)!
-	//DrawHexagonGridDistance(V3_ONE, V3_ZERO, 10.0f, 0.1f);
-	//SetPipelineState(gPipelineDefault);
-
-	BindTexture2D(fTextureWhite, 0);
 	BindShader(&gShaderDeferred);
 
+	world = Scale(100.0f * SCREEN_ASPECT, 100.0f, 1.0f);
+	mvp = world * gView * gProj;
 	SendMat4("u_mvp", mvp);
 	SendMat4("u_world", world);
 	SendMat3("u_normal", NormalMatrix(world));
 	SendInt("u_tex", 0);
+	SetPipelineState(gPipelineNoDepth);
+	BindTexture2D(GetHexagonGrid(), 0);
+	DrawMesh(gMeshPlane);
+	UnbindTexture2D(GetHexagonGrid(), 0);
+	SetPipelineState(gPipelineDefault);
+
+
+	world = MatrixIdentity();
+	mvp = world * gView * gProj;
+	SendMat4("u_mvp", mvp);
+	SendMat4("u_world", world);
+	SendMat3("u_normal", NormalMatrix(world));
+	BindTexture2D(fTextureWhite, 0);
 	DrawMesh(gMeshTd);
+	UnbindTexture2D(fTextureWhite, 0);
 
 	UnbindShader();
-	UnbindTexture2D(fTextureWhite, 0);
 	UnbindFramebuffer(fGeometryBuffer);
 
 	BindTexture2D(fGeometryBuffer.colors[0], 0);
@@ -103,8 +96,8 @@ void NeonDriveScene::OnDraw()
 	BindTexture2D(fGeometryBuffer.colors[2], 2);
 	BindShader(&gShaderDeferredLighting);
 
-	Matrix S = Scale(V3_ONE * 10.0f);
-	Matrix R = LookRotation(fLightPosition, gCamera.position, V3_UP);
+	Matrix S = Scale(V3_ONE * 50.0f);
+	Matrix R = MatrixIdentity();
 	Matrix T = Translate(fLightPosition);
 	world = S * R * T;
 	mvp = world * gView * gProj;
@@ -114,11 +107,12 @@ void NeonDriveScene::OnDraw()
 	SendInt("u_albedo", 2);
 	SendVec3("u_lightPosition", fLightPosition);
 	SendVec3("u_lightColor", fLightColor);
-	SendFloat("u_ambient", 1.0f);
+	SendFloat("u_ambient", 0.25f);
 	SendFloat("u_diffuse", 1.0f);
 	SendVec2("u_viewportSize", { SCREEN_WIDTH, SCREEN_HEIGHT });
 	SendVec2("u_viewportOffset", V2_ZERO);
 
+	// TODO -- change to depth GL_GEQUAL & sphere rendering to solve volume issue
 	SetPipelineState(gPipelineNoDepth);
 	DrawMesh(gMeshCircle);
 	SetPipelineState(gPipelineDefault);
@@ -127,6 +121,4 @@ void NeonDriveScene::OnDraw()
 	UnbindTexture2D(fGeometryBuffer.colors[2], 2);
 	UnbindTexture2D(fGeometryBuffer.colors[1], 1);
 	UnbindTexture2D(fGeometryBuffer.colors[0], 0);
-
-	//DrawMeshWireframes(gMeshCircle, world, fLightColor);
 }
