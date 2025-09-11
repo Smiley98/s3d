71ac3cc7feaf7320780660c1a5c60ebd842a4315
Smiley98 <smileysofficialcinema@gmail.com>
2025-09-11 14:41:40 -0400

    Changed resolution to be square

--- src/Config.h
+++ src/Config.h
@@ -1,4 +1,4 @@
 #pragma once
-constexpr float SCREEN_WIDTH = 1280.0f;
-constexpr float SCREEN_HEIGHT = 720.0f;
+constexpr float SCREEN_WIDTH = 800.0f;
+constexpr float SCREEN_HEIGHT = 800.0f;
 constexpr float SCREEN_ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT;
